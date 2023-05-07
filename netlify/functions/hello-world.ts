5ec7cd193e2a1e88490161e5d13f8cefff65c67f
Taevas <67872932+TTTaevas@users.noreply.github.com>
2023-05-07 02:13:48 +0200

    Setup something simple and straight-forward

---
+++ netlify/functions/hello-world.ts
@@ -0,0 +1,33 @@
+import { Handler } from '@netlify/functions'
+import { api } from "./shared/api"
+
+const handler: Handler = async (event, context) => {
+  console.log(event)
+  let lastfm = await api<{
+    recenttracks: {
+      track: {
+        artist: {
+          "#text": string
+        },
+        image: {
+          size: string,
+          "#text": string
+        }[]
+        album: {
+          "#text": string
+        },
+        name: string,
+        "@attr"?: {
+          nowplaying?: string
+        }
+      }[]
+    }
+  }>
+  (`http://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=TTTaevas&api_key=${process.env["API_LASTFM"]}&format=json&limit=1`)
+  return {
+    statusCode: 200,
+    body: JSON.stringify(lastfm.recenttracks.track[0])
+  }
+}
+
+export { handler }
---
+++ netlify/functions/shared/api.ts
@@ -0,0 +1,10 @@
+// Standard variation
+export async function api<T>(url: string): Promise<T> {
+  return fetch(url)
+    .then(response => {
+      if (!response.ok) {
+        throw new Error(response.statusText)
+      }
+      return response.json() as Promise<T>
+    })
+}
