3072c8c88d2c0a437e9e56f308614c111645dc38
Taevas <67872932+TTTaevas@users.noreply.github.com>
2024-04-30 17:15:16 +0200

    Small adjustments
    `SocialButton` and its image are no longer draggable
    It's no longer possible to click the edge of a `SocialButton` without clicking the hyperlink
    Any `SocialButton` will darken while `active`
    `Tab` has its name and close button switch positions
    
    The reasoning for this last change is because it's more intuitive (to Windows and most Linux users)
    and because it makes more sense to have the mouse be on the middle/right of the screen surrounded by buttons, rather than the left where there's very little
    
    tldr; my interpretation of psychology

--- src/components/SocialButton.tsx
+++ src/components/SocialButton.tsx
@@ -16,15 +16,15 @@ function SocialButton({
   padding?: string;
 }) {
   return (
-    <button title={title} className={`
-    m-1 h-12 w-12 bg-white
-    rounded-full border-solid hover:border-dashed border-4 hover:border-8 ${border}
-    transition-transform duration-75 ${rotation} hover:rotate-0
-    `}>
-      <a href={link} target="_blank" rel="noreferrer">
-        <img className={`${padding} w-32 absolute right-1/2 bottom-1/2 translate-x-2/4 translate-y-2/4`} src={image} alt={title}/>
-      </a>
-    </button>
+    <a draggable="false" href={link} target="_blank" rel="noreferrer">
+      <button title={title} className={`
+      m-1 h-12 w-12 bg-white
+      rounded-full border-solid hover:border-dashed active:brightness-95 border-4 hover:border-8 ${border}
+      transition-transform duration-75 ${rotation} hover:rotate-0
+      `}>
+        <img draggable="false" className={`${padding} w-32 absolute right-1/2 bottom-1/2 translate-x-2/4 translate-y-2/4`} src={image} alt={title}/>
+      </button>
+    </a>
   );
 }
 
